f478eec3f00e4296cd5ed5101f561d9f5d6ed80e
Jachin Jiang <jachin.jiang@gmail.com>
2023-02-03 14:18:49 +0800

    successfully run locally, and modify config.yaml(CIFAR10)

--- model/resnet_cifar.py
+++ model/resnet_cifar.py
@@ -121,7 +121,7 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = F.avg_pool2d(out, out.size()[3])
+        out = F.avg_pool2d(out, (out.size()[3], out.size()[3]))
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
